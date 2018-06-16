b3337b3964aa452989f63b84ebd2e1ede9f003ad
Maulana Yusup Abdullah <maulanayusupp@gmail.com>
2018-06-16 22:12:49 +0700

    Update

---
+++ src/components/dashboard/dashboard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="dashboard">
+    <h1>That's the dashboard!</h1>
+    <p>You should only get here if you're authenticated!</p>
+    <p v-if="user">Your email address: {{ user.email }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    computed: {
+      user() {
+        var user = this.$store.getters.user ? this.$store.getters.user : false;
+        console.log(user);
+        return user
+      },
+    },
+    created () {
+      this.$store.dispatch('fetchUser')
+    },
+  }
+</script>
+
+<style scoped>
+  h1, p {
+    text-align: center;
+  }
+
+  p {
+    color: red;
+  }
+</style>
